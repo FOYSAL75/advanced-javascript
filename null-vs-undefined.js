4b1c10cf3d13c4f0d929a21a5460d9007bc9a05a
foysal75 <foysalahmedofficial7575@gmail.com>
2021-05-03 18:23:03 +0600

    i know about null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,10 @@
+// function add(num1, num2){
+//     console.log(num1 + num2);
+//     return ;
+// }
+
+// function add(num1, num2){
+//     console.log(num1, num2);
+// }
+const man = {name:"mofiz",age:34, work:"yes"}
+console.log(man.gf);

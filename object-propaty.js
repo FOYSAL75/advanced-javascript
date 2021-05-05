c2ef75dba5f40220884190382719b34e66f3502f
foysal75 <foysalahmedofficial7575@gmail.com>
2021-05-05 06:44:23 +0600

    Onek shikhlam

---
+++ object-propaty.js
@@ -0,0 +1,13 @@
+const students = [
+    { id: 21, name: 'omor sany'},
+    { id: 31, name: 'manna'},
+    { id: 41, name: 'moyori'},
+    { id: 51, name: "dipjol"}
+]
+
+// const namesOne = students.map(x => x.id);
+
+const namesTwo = students.filter(x => x.id>30);
+
+const namesThree = students.find(x => x.id>30);
+console.log(namesThree);
